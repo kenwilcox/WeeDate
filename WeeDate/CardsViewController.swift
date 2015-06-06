e63a04c4bdf054eb49748fa02f1eef5074b25732
Ken Wilcox <wilcoxk@gmail.com>
2015-06-05 22:03:53 -0600

    Added CardViewController

---
+++ WeeDate/CardsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CardsViewController.swift
+//  WeeDate
+//
+//  Created by Kenneth Wilcox on 6/5/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+  @IBOutlet weak var cardStackView: UIView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
+}
