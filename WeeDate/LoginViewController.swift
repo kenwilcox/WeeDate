76241b5026ddd5858654790b2f9c8f4c32a4969f
Ken Wilcox <wilcoxk@gmail.com>
2015-06-12 23:45:55 -0600

    created login view controller

---
+++ WeeDate/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  WeeDate
+//
+//  Created by Kenneth Wilcox on 6/12/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func pressedFBLogin(sender: UIButton) {
+
+  }
+}
