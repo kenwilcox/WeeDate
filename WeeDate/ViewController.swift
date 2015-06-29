79671e2dd031c772e744374e1436fdbeb9704d2f
Ken Wilcox <wilcoxk@gmail.com>
2015-06-28 22:01:36 -0600

    setting up view controllers

--- WeeDate/ViewController.swift
+++ WeeDate/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class ViewController: UIPageViewController {
   
+  let cardsVC: UIViewController! = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("CardsNavController") as! UIViewController
+  let profileVC: UIViewController! = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ProfileNavController") as! UIViewController
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
     view.backgroundColor = UIColor.whiteColor()
     dataSource = self
+    
+    self.setViewControllers([cardsVC], direction: .Forward, animated: true, completion: nil)
   }
   
   override func didReceiveMemoryWarning() {
@@ -28,11 +33,21 @@ class ViewController: UIPageViewController {
 extension ViewController: UIPageViewControllerDataSource {
   func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
     
-    return nil
+    switch viewController {
+    case cardsVC:
+      return profileVC
+    default:
+      return nil
+    }
   }
   
   func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
     
-    return nil
+    switch viewController {
+    case profileVC:
+      return cardsVC
+    default:
+      return nil
+    }
   }
 }
